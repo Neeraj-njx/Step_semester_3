098ec65ff41da6a9856b37ecf136c607e128a381
Neeraj-njx <nvn7331@gmail.com>
2026-09-05 11:29:11 +0530

    Week3 classpro 5

---
+++ classproblem/StudentDemo.java
@@ -0,0 +1,34 @@
+class Student {
+    // Instance fields
+    String name;
+    int attendance;
+
+    // Static fields
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    // Constructor
+    Student(String name, int attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    // Static method
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+}
+
+public class StudentDemo {
+    public static void main(String[] args) {
+
+        // Create two Student objects
+        Student student1 = new Student("Ravi", 90);
+        Student student2 = new Student("Anitha", 95);
+
+        // Call static method using class name
+        Student.printCollegeInfo();
+    }
+}
