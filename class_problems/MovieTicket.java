65e84d7c1c68db9a6cbe8093c0120de50f7a32e6
Neeraj-njx <nvn7331@gmail.com>
2026-09-05 09:50:35 +0530

    Week5 classproblem 1

---
+++ class_problems/MovieTicket.java
@@ -0,0 +1,45 @@
+class MovieTicket {
+    private String seatNumber;
+    String screenId;              // default/package-private
+    protected double ticketPrice;
+    public String movieTitle;
+
+    static String classifyAccess(String fieldModifier, String accessorContext) {
+        switch (fieldModifier) {
+            case "private":
+                return accessorContext.equals("SAME_CLASS")
+                        ? "ALLOWED" : "DENIED";
+
+            case "default":
+                return (accessorContext.equals("SAME_CLASS")
+                        || accessorContext.equals("SAME_PACKAGE"))
+                        ? "ALLOWED" : "DENIED";
+
+            case "protected":
+                return (accessorContext.equals("SAME_CLASS")
+                        || accessorContext.equals("SAME_PACKAGE"))
+                        ? "ALLOWED" : "DENIED";
+
+            case "public":
+                return "ALLOWED";
+
+            default:
+                return "DENIED";
+        }
+    }
+
+    static String summarizeBatch(String[][] attempts) {
+        int allowed = 0;
+        int denied = 0;
+
+        for (String[] attempt : attempts) {
+            if (classifyAccess(attempt[0], attempt[1]).equals("ALLOWED")) {
+                allowed++;
+            } else {
+                denied++;
+            }
+        }
+
+        return "Allowed: " + allowed + " | Denied: " + denied;
+    }
+}
