8be2102a5127233447f93ef77e4f6e906b7f80f2
Neeraj-njx <nvn7331@gmail.com>
2026-09-11 16:53:44 +0530

    Week5 Assigment2

---
+++ assigment_problems/LibraryMember.java
@@ -0,0 +1,13 @@
+public class LibraryMember {
+
+    private String membershipPin;
+
+    String branchCode;
+
+    String finesOwed;
+
+    public String displayName;
+
+    public LibraryMember() {
+    }
+}
