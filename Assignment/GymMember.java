23e410e4b5ff7bf63dda132e7f6b34a8e64927eb
Neeraj-njx <nvn7331@gmail.com>
2026-09-18 21:09:43 +0530

    Week6 assignment pro 1

---
+++ Assignment/GymMember.java
@@ -0,0 +1,47 @@
+class GymMember {
+    protected String memberId;
+    protected int monthlyFee;
+    protected int sessionsAttended;
+
+    public GymMember(String memberId, int monthlyFee) {
+        if (memberId == null || memberId.trim().length() < 4) {
+            throw new IllegalArgumentException();
+        }
+
+        this.memberId = memberId;
+        this.monthlyFee = monthlyFee;
+    }
+
+    public void attendSession() {
+        sessionsAttended++;
+    }
+
+    public int getSessionsAttended() {
+        return sessionsAttended;
+    }
+
+    public static String signUpBatch(String[] memberIds, int monthlyFee) {
+        int signedUp = 0;
+        int rejected = 0;
+
+        for (String memberId : memberIds) {
+            try {
+                new GymMember(memberId, monthlyFee);
+                signedUp++;
+            } catch (IllegalArgumentException e) {
+                rejected++;
+            }
+        }
+
+        return "Signed Up: " + signedUp + " | Rejected: " + rejected;
+    }
+}
+
+class PremiumMember extends GymMember {
+    private String trainerName;
+
+    public PremiumMember(String memberId, int monthlyFee, String trainerName) {
+        super(memberId, monthlyFee);
+        this.trainerName = trainerName;
+    }
+}
