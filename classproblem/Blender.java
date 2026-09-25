4cf095291b107d2351eb5b5dcefdbadd9d7cf9e0
Neeraj-njx <nvn7331@gmail.com>
2026-09-25 23:36:22 +0530

    week7 pro4

---
+++ classproblem/Blender.java
@@ -0,0 +1,36 @@
+abstract class KitchenTool {
+    private int speedLevel;
+
+    public abstract String prepare();
+
+    public int getSpeedLevel() {
+        return speedLevel;
+    }
+
+    public void setSpeedLevel(int speedLevel) {
+        if (speedLevel >= 1 && speedLevel <= 5) {
+            this.speedLevel = speedLevel;
+        }
+    }
+}
+
+interface Washable {
+    String clean();
+}
+
+class Blender extends KitchenTool implements Washable {
+
+    public Blender() {
+        setSpeedLevel(1);
+    }
+
+    @Override
+    public String prepare() {
+        return "Blending at speed " + getSpeedLevel();
+    }
+
+    @Override
+    public String clean() {
+        return "Blender rinsed and dried";
+    }
+}
